8862bd31e929b517ffbe819d87b2bfb3284bccd0
Baggio Shehadi <k3nh1n@yahoo.com>
2017-05-02 14:52:53 -0500

    fixed table schema

--- models/user.js
+++ models/user.js
@@ -1,93 +1,91 @@
 module.exports = function(sequelize, Sequelize) {
- 
-    var User = sequelize.define('user', {
- 
-        id: {
-            autoIncrement: true,
-            primaryKey: true,
-            type: Sequelize.INTEGER
-        },
- 
-        firstname: {
-            type: Sequelize.STRING,
-            notEmpty: true
-        },
- 
-        lastname: {
-            type: Sequelize.STRING,
-            notEmpty: true
-        },
- 
-        username: {
-            type: Sequelize.TEXT
-            allowNull: false
-        },
- 
-        about: {
-            type: Sequelize.TEXT
-        },
- 
-        email: {
-            type: Sequelize.STRING,
-            validate: {
-                isEmail: true
-            }
-        },
- 
-        password: {
-            type: Sequelize.STRING,
-            allowNull: false
-        },
- 
-        last_login: {
-            type: Sequelize.DATE
-        },
- 
-        status: {
-            type: Sequelize.ENUM('active', 'inactive'),
-            defaultValue: 'active'
-        },
 
-        graduation: {
-            type: Sequelize.DATE,
-            allowNull: false
-        },
+	var User = sequelize.define('user', {
 
-        linkedIn_url: {
-            type: Sequelize.STRING
+		id: {
+			autoIncrement: true,
+			primaryKey: true,
+			type: Sequelize.INTEGER
+		},
 
-        },
+		firstname: {
+			type: Sequelize.STRING,
+			notEmpty: true
+		},
 
-        profile_photo: {
-            type: Sequelize.STRING
+		lastname: {
+			type: Sequelize.STRING,
+			notEmpty: true
+		},
 
-        },
+		username: {
+			type: Sequelize.TEXT
+		},
 
-        personal_website: {
-            type: Sequelize.STRING
-        },
+		about: {
+			type: Sequelize.TEXT
+		},
 
-        first_salary: {
-            type: Sequelize.STRING
-        },
+		email: {
+			type: Sequelize.STRING,
+			validate: {
+				isEmail: true
+			}
+		},
 
-        first_job: {
-            type: Sequelize.STRING
-        },
+		password: {
+			type: Sequelize.STRING,
+			allowNull: false
+		},
 
-        interview_time: {
-            type: Sequelize.STRING
-        },
+		last_login: {
+			type: Sequelize.DATE
+		},
 
-        mentor: {
-            type: Sequelize.Boolean
-        },
-        
-        user_comment: {
-            type: Sequelize.TEXT
-        }
-    });
- 
-    return User;
- 
+		status: {
+			type: Sequelize.ENUM('active', 'inactive'),
+			defaultValue: 'active'
+		},
+
+		graduation: {
+			type: Sequelize.DATE
+		},
+
+		linkedIn_url: {
+			type: Sequelize.STRING
+
+		},
+
+		profile_photo: {
+			type: Sequelize.STRING
+
+		},
+
+		personal_website: {
+			type: Sequelize.STRING
+		},
+
+		first_salary: {
+			type: Sequelize.STRING
+		},
+
+		first_job: {
+			type: Sequelize.STRING
+		},
+
+		interview_time: {
+			type: Sequelize.STRING
+		},
+
+		mentor: {
+			type: Sequelize.BOOLEAN
+		},
+
+		user_comment: {
+			type: Sequelize.TEXT
+		}
+	});
+
+	return User;
+
 }
