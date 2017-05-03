aa2f6bbb51567865fedc639359e9a29eaaf213e3
Dacia Yang <yang.dacia@gmail.com>
2017-05-03 13:28:56 -0500

    dashboard.js edits

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -9,10 +9,11 @@ $( document ).ready(function() {
             rows_area.attr("id", "user_row" + i);
             $(".individual-result").append(rows_area);
 
-            $("#user_row" + i).append("<h2>" + data[i].firstname + " " + data[i].lastname + "</h2>");
-            $("#user_row" + i).append("<h3>Employment status: " + data[i].employment + "</h4>");
-            $("#user_row" + i).append("<h3>City: " + data[i].location+ "</h4>");
-            $("#user_row" + i).append("<h3>Cohort: " + data[i].cohort + "</h4>");
+             $("#user_row" + i).append("<img src =" + "'" + data[1][i].profilePic + "'" + ">");
+            $("#user_row" + i).append("<h2>" + data[1][i].firstname + " " + data[1][i].lastname + "</h2>");
+            $("#user_row" + i).append("<h3>Employment status: " + data[1][i].employment + "</h4>");
+            $("#user_row" + i).append("<h3>City: " + data[1][i].location+ "</h4>");
+            $("#user_row" + i).append("<h3>Cohort: " + data[1][i].cohort + "</h4>");
           }
         });// done
 	});// end of document ready
