3da59d870300b6c063667f3a039b4493275f528a
Anastasia Marie <ana.goodwin10@gmail.com>
2017-05-04 09:30:11 -0500

    about page landing page edits

--- public/js/frontend.js
+++ public/js/frontend.js
@@ -81,3 +81,16 @@ previousFieldset.animate({
 	})
 
 });
+
+// smooth scroll 
+
+
+$('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+});
