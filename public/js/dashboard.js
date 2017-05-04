27eea099ab3ed4a9cc06d033adf22117fcac00be
Dacia Yang <yang.dacia@gmail.com>
2017-05-04 09:58:41 -0500

    linkedIn URL working

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -9,20 +9,21 @@ $( document ).ready(function() {
             rows_area.attr("id", "user_row" + i);
             $(".allRows").append(rows_area);
 
-             $("#user_row" + i).append("<img src =" + "'" + data[1][i].profilePic + "'" + ">");
+             $("#user_row" + i).append("<img src =" + "'" + data[1][i].profilePic + "'" + "/>");
             $("#user_row" + i).append("<h2>" + data[1][i].firstname + " " + data[1][i].lastname + "</h2>");
-            $("#user_row" + i).append("<h3>Employment status: " + data[1][i].employment + "</h4>");
-            $("#user_row" + i).append("<h3>City: " + data[1][i].location+ "</h4>");
-            $("#user_row" + i).append("<h3>Cohort: " + data[1][i].cohort + "</h4>");
+            $("#user_row" + i).append("<h3>Employment status: " + data[1][i].employment + "</h3>");
+            $("#user_row" + i).append("<h3>City: " + data[1][i].location+ "</h3>");
+            $("#user_row" + i).append("<h3>Cohort: " + data[1][i].cohort + "</h3>");
           }
 
           for (var i = 0; i < 1; i++){
-            //$(".profile-image").attr(src,"'" + data[0].profilePic + "'");
+           
             $(".user-fullname").html(data[0].firstname + " " + data[0].lastname);
             $(".user-city").html("").html(data[0].location);
             $(".user-cohort").html("").html(data[0].cohort);
-            $(".user-linkedin").html("").html(data[0].linkedinURL);
+            $(".user-linkedin").html("").html(data[0].linkedInURL);
             $(".user-profile").html("").html(data[0].profileURL);
+             // $(".profile-image").attr(src,"'" + data[0].profilePic + "'");
           }
 
         });// done
