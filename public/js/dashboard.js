3d0372c64c28b9c06179bd89dcc83c573b369c0a
Jillian Flook <jkenw10@gmail.com>
2017-05-05 09:58:09 -0500

    change index signup form to connect to post auth route to main signup form

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -3,21 +3,20 @@ $( document ).ready(function() {
       url:'/api/users',
       method:'GET'
     }).done(function(data){
-           for (var i = 0; i < data[1].length; i++) {
+          // loop through DB to create rows for all users with their information
+          for (var i = 0; i < data[1].length; i++) {
             var rows_area = $("<div class='individual-result well' data-toggle='modal' data-target='#myModal'>");
-            //rows_area.addClass("row_area");
             rows_area.attr("id", "user_row" + i);
             $(".allRows").append(rows_area);
-
-             $("#user_row" + i).append("<img src = '" + data[1][i].profilePic + "'/>");
+            $("#user_row" + i).append("<img src = '" + data[1][i].profilePic + "'/>");
             $("#user_row" + i).append("<h2>" + data[1][i].firstname + " " + data[1][i].lastname + "</h2>");
             $("#user_row" + i).append("<h3>Employment status: " + data[1][i].employment + "</h3>");
             $("#user_row" + i).append("<h3>City: " + data[1][i].location+ "</h3>");
             $("#user_row" + i).append("<h3>Cohort: " + data[1][i].cohort + "</h3>");
           }
 
-          for (var i = 0; i < 1; i++){
-           
+          
+          //add logged-in user data to the left panel on dashboard.html  
             $(".user-fullname").html(data[0].firstname + " " + data[0].lastname);
             $(".user-city").html("Location: " + data[0].location);
             $(".user-cohort").html("Graduation: " + data[0].cohort);
