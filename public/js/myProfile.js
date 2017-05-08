187b873ff82e2cb9a127fd069b4b8f8477a97bf3
Anastasia Marie <ana.goodwin10@gmail.com>
2017-05-07 19:03:27 -0500

    autofill form

--- public/js/myProfile.js
+++ public/js/myProfile.js
@@ -7,6 +7,7 @@ $( document ).ready(function() {
           //add logged-in user data to the left panel on dashboard.html  
             $(".edit-fullname").html(data.firstname + " " + data.lastname);
             $(".edit-location").html(data.location);
+            $(".edit-about").html(data.about);
             $(".edit-status").html(data.status);
             console.log(data.status)
             $(".edit-employer").html(data.employer);
@@ -15,9 +16,33 @@ $( document ).ready(function() {
             $(".edit-cohort").html(data.cohort);
             $(".edit-email").html(data.email);
             $(".edit-linkedin").html(data.linkedInURL);
-            $(".edit-profile").html(data.portfolioURL);
-            $(".edit-mentor").html(data.portfolioURL);
+            $(".edit-portfolioURL").html(data.portfolioURL);
+            $(".edit-mentor").html(data.mentor);
             $(".profile-image").attr("src", data.profilePic);
+
+        });// done
+  });// end of document ready
+
+$(".edit").on("click", function() {
+        $.ajax({
+      url:'/api/currentUser',
+      method:'GET'
+    }).done(function(data){
+        
+          //add logged-in user data to the left panel on dashboard.html  
+            $(".firstname").val(data.firstname);
+            $(".lastname").val(data.lastname);
+            $(".location").val(data.location);
+            $(".about").val(data.about);
+            $(".status").val(data.status);
+            console.log(data.status)
+            $(".employer").val(data.employer);
+            $(".first_salary").val(data.first_salary);
+            $(".interview_time").val(data.interview_time);
+            $(".linkedInURL").val(data.linkedInURL);
+            $(".portfolioURL").val(data.portfolioURL);
+            $(".mentor").val(data.mentor);
+            $(".profilePic").val(data.profilePic);
 
         });// done
   });// end of document ready
