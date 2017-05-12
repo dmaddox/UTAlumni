79b71a67f1e2083b68440bc99d9bbb72e6ee3c3e
Baggio Shehadi <k3nh1n@yahoo.com>
2017-05-12 09:03:57 -0500

    added a div for dashboard and fixed search.js

--- public/js/search.js
+++ public/js/search.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {
-	$("#drop-down-search").on("change", function() {
-		var dropFilter
-		dropFilter = $("#drop-down-search").val().trim();
-		console.log(dropFilter)
-		if (dropFilter === "cohort") {
-			console.log("caught chenge to cohort");
-			$("#userFilterElement").html('<select id="userFilter" class="form-control selectpicker" data-style="btn-primary"><option value="Fall Full Time 2016">Fall Full Time 2016</option><option value="Fall Part Time 2016">Fall Part Time 2016</option><option value="Spring Full Time 2017">Spring Full Time 2017</option><option value="Spring Part Time 2017">Spring Part Time 2017</option><option value="Fall Full Time 2017">Fall Full Time 2017</option></select>');
+  $("#drop-down-search").on("change", function() {
+    var dropFilter;
+    dropFilter = $("#drop-down-search").val().trim();
+    console.log(dropFilter);
+    if (dropFilter === "cohort") {
+      console.log("caught chenge to cohort");
+      $(".searchBarDiv").html(`<select id="userFilter" class="form-control selectpicker" data-style="btn-primary"><option value="Fall Full Time 2016">Fall Full Time 2016</option><option value="Fall Part Time 2016">Fall Part Time 2016</option><option value="Spring Full Time 2017">Spring Full Time 2017</option><option value="Spring Part Time 2017">Spring Part Time 2017</option><option value="Fall Full Time 2017">Fall Full Time 2017</option></select>`);
 
-		} else if (dropFilter === "state") {
-			$('#userFilterElement').html(`<select class="form-control" id="userFilter" name="state">
+    } else if (dropFilter === "state") {
+      $('.searchBarDiv').html(`
+		<select class="form-control" id="userFilter" name="state">
 			<option value="">N/A</option>
 			<option value="AK">Alaska</option>
 			<option value="AL">Alabama</option>
@@ -63,38 +64,39 @@ $(document).ready(function() {
 			<option value="WV">West Virginia</option>
 			<option value="WY">Wyoming</option>
 		</select>`);
-		} else {
-			console.log("hit the else");
-			$("#userFilterElement").html('<input id="userFilter" type="text" class=" form-control" placeholder="Search"/>')
-		}
-	})
-
-
-
-	$("#searchBtn").on("click", function() {
-		var dropFilter;
-		var userFilter;
-		dropFilter = $("#drop-down-search").val().trim();
-		userFilter = $("#userFilter").val().trim();
-		console.log(dropFilter + " " + userFilter)
-		if (dropFilter === "") {
-
-		} else {
-			$(".individual-result").each(function(index) {
-				var divResult;
-				console.log(this);
-				// THIS IS THE QUESTION- how can we targe the h3 w/i a div and get the value
-				if (dropFilter === "city" || dropFilter === "state") {
-					divResult = $(this).children("#searchDiv").children('#location').children('#'+dropFilter).html().trim();
-				} else {
-					divResult = $(this).children("#searchDiv").children('#' + dropFilter).html().trim();
-				}
-				if (divResult.toLowerCase() !== userFilter.toLowerCase()) {
-					$(this).hide();
-				} else {
-					$(this).show();
-				}
-			})
-		}
-	})
-})
+    } else {
+      console.log("hit the else");
+      $(".searchBarDiv").html(`
+		<input id="userFilter" type="text" class="search-input form-control" placeholder="Search">`);
+    }
+  });
+  $("#reset-button").on("click", function() {
+    $(".individual-result").each(function(index) {
+      $(this).show();
+    });
+  });
+  $("#searchBtn").on("click", function() {
+    var dropFilter;
+    var userFilter;
+    dropFilter = $("#drop-down-search").val().trim();
+    userFilter = $("#userFilter").val().trim();
+    console.log(dropFilter + " " + userFilter);
+    if (dropFilter === "") {} else {
+      $(".individual-result").each(function(index) {
+        var divResult;
+        console.log(this);
+        // THIS IS THE QUESTION- how can we targe the h3 w/i a div and get the value
+        if (dropFilter === "city" || dropFilter === "state") {
+          divResult = $(this).children("#searchDiv").children('#location').children('#' + dropFilter).html().trim();
+        } else {
+          divResult = $(this).children("#searchDiv").children('#' + dropFilter).html().trim();
+        }
+        if (divResult.toLowerCase() !== userFilter.toLowerCase()) {
+          $(this).hide();
+        } else {
+          $(this).show();
+        }
+      });
+    }
+  });
+});
