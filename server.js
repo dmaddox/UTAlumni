224cacb01dd4f096b2afb758e24856c04d9c30c5
Baggio Shehadi <k3nh1n@yahoo.com>
2017-05-12 14:43:59 -0500

    added stats schema and deleted handlebars

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 var passport = require('passport');
 var session = require('express-session');
 var bodyParser = require('body-parser');
+var CronJob = require('cron').CronJob;
 var env = require('dotenv').load();
 	//var exphbs = require('express-handlebars')
 
@@ -33,6 +34,114 @@ app.use(express.static("./public"));
 
 //Model
 var models = require("./models");
+// every saturday create a field to stats database
+var CronJob = require('cron').CronJob;
+var job = new CronJob({
+  cronTime: '0 8 * * 6',
+  onTick: function() {
+		var inTech;
+    var employedOutTech;
+    var studentNum;
+    var noJobNum;
+    var dataObject;
+    models.user.findAll({
+      where: {
+        status: "employed-tech"
+      }
+    }).then(function(data) {
+      inTech = data.length;
+      models.user.findAll({
+        where: {
+          status: "employed-else"
+        }
+      }).then(function(employedElse) {
+        employedOutTech = employedElse.length;
+        models.user.findAll({
+          where: {
+            status: "current-student"
+          }
+        }).then(function(current) {
+          studentNum = current.length;
+          models.user.findAll({
+            where: {
+              status: "seeking"
+            }
+          }).then(function(seeking) {
+            noJobNum = seeking.length;
+            models.user.findAll({
+              where: {
+                $or: [
+                  {
+                    interview_time: "0 months"
+                  }, {
+                    interview_time: "1 months"
+                  }, {
+                    interview_time: "2 months"
+                  }, {
+                    interview_time: "3 months"
+                  }
+                ]
+              }
+            }).then(function(zeroToThree) {
+              var zeroNum = zeroToThree.length;
+              models.user.findAll({
+                where: {
+                  $or: [
+                    {
+                      interview_time: "4 months"
+                    }, {
+                      interview_time: "5 months"
+                    }, {
+                      interview_time: "6 months"
+                    }, {
+                      interview_time: "7 months"
+                    }
+                  ]
+                }
+              }).then(function(fourToSeven) {
+                var fourNum = fourToSeven.length;
+                models.user.findAll({
+                  where: {
+                    $or: [
+                      {
+                        interview_time: "8 months"
+                      }, {
+                        interview_time: "9 months"
+                      }, {
+                        interview_time: "10 months"
+                      }, {
+                        interview_time: "11 months"
+                      }, {
+                        interview_time: "12 months"
+                      }
+                    ]
+                  }
+                }).then(function(eightToTwelve) {
+                  var eightNum = eightToTwelve.length;
+									dataObject = {
+										employedTech: inTech,
+										employedElse: employedOutTech,
+										studentNum: studentNum,
+										noJob: noJobNum,
+										zeroToThree: zeroNum,
+										fourToSeven: fourNum,
+										eightToTwelve: eightNum,
+										date: Date.now()
+									};
+									models.statistic.create(dataObject).then(function(){
+										console.log("stats updated");
+									});
+                });
+              });
+            });
+          });
+        });
+      });
+    });
+  },
+  start: true,
+  timeZone: 'America/Los_Angeles'
+});
 //Routes
 var authRoute = require('./routes/auth.js')(app, passport);
 //load passport strategies
